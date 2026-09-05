3e28a7ef1594802c837859e20558f32b7bebe3d7
FStrony <fstrony@gmail.com>
2026-09-05 16:32:25 -0300

    feat(seo): add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://fstrony.github.io',
+  integrations: [
+    sitemap({
+      filter: (page) => new URL(page).pathname !== '/',
+    }),
+  ],
   i18n: {
     locales: ['en', 'pt-BR'],
     defaultLocale: 'en',
